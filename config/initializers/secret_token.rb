8625f7e8026e3dad7f56da4be6908fce3f5edb3f
Albert Hung <ahung88@gmail.com>
2014-01-15 09:34:28 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvernoteClone::Application.config.secret_token = '7e3b5ea63c8c782f80afc921b86b4666bc767ad40c099c1cd1cbf0193ee1be41c856c6693b1e439cf5bf62d3cceb2747479ad66de13944301a505b44daf3ef0e'
